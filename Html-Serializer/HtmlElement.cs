32540be37bc2cd0b7899e09b5ec9dbfda0cb70b6
ללי קלינמן <smalot2247@gmail.com>
2025-01-22 23:54:35 +0200

    first

---
+++ Html-Serializer/HtmlElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Html_Serializer
+{
+    internal class HtmlElement
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Attributes { get; set; }
+        public List<string> Classes { get; set; }
+        public string InnerHtml { get; set; }
+        public HtmlElement Parent { get; set; }
+        public HtmlElement Children { get; set; }
+
+    }
+}
---
+++ Html-Serializer/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Text.RegularExpressions;
+
+async Task<string> Load(string url)
+{
+    HttpClient client = new HttpClient();
+    var response = await client.GetAsync(url);
+    var html = await response.Content.ReadAsStringAsync();
+    return html;
+}
+
+
+var html = await Load("https://hebrewbooks.org/");//loading html from website
+
+var cleanhtml = new Regex("\\s").Replace(html,"");
+var htmlLines = new Regex("<(.*?)>").Split(cleanhtml).Where(s=>s.Length>0);
+Console.WriteLine();
+
